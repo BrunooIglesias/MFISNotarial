8e0e41bf4855912bdcc5fe3e6319e335a9a9fbb7
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2025-04-08 00:18:36 -0300

    refactor: name page refactor

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,12 +5,21 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         component: HomePage,
+        meta: {
+            title: 'Mary Iglesias Escribana - Inicio'
+        }
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.afterEach((to) => {
+    if (to.meta?.title) {
+        document.title = to.meta.title
+    }
 })
 
 export default router
